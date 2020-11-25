6e5209cf9049127269a32d7debe8d721fe8a6c19
42 <33488710+FortyTwoFortyTwo@users.noreply.github.com>
2020-11-25 23:34:52 +0000

    More GitHub actions fix

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,11 +3,11 @@ cd build/addons/sourcemod/scripting
 
 # Get plugin version
 export PLUGIN_VERSION=$(sed -En '/#define PLUGIN_VERSION\W/p' superzombiefortress.sp)
-echo ::set-env name=PLUGIN_VERSION::$(echo $PLUGIN_VERSION | grep -o '[0-9]*\.[0-9]*\.[0-9]*')
+echo "PLUGIN_VERSION=(echo $PLUGIN_VERSION | grep -o '[0-9]*\.[0-9]*\.[0-9]*')" >> $GITHUB_ENV
 
 # Get revision
 export PLUGIN_VERSION_REVISION=$(git rev-list --count HEAD)
-echo ::set-env name=PLUGIN_VERSION_REVISION::$PLUGIN_VERSION_REVISION
+echo "PLUGIN_VERSION_REVISION=(git rev-list --count HEAD)" >> $GITHUB_ENV
 
 # Set revision to superzombiefortress.sp
 sed -i -e 's/#define PLUGIN_VERSION_REVISION.*".*"/#define PLUGIN_VERSION_REVISION "'$PLUGIN_VERSION_REVISION'"/g' superzombiefortress.sp
