fc3afec7584de6ecd0b8fbe58ec10eb066a359e3
42 <33488710+FortyTwoFortyTwo@users.noreply.github.com>
2020-11-25 23:50:48 +0000

    q

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,11 +3,14 @@ cd build/addons/sourcemod/scripting
 
 # Get plugin version
 export PLUGIN_VERSION=$(sed -En '/#define PLUGIN_VERSION\W/p' superzombiefortress.sp)
-echo "PLUGIN_VERSION=(echo $PLUGIN_VERSION | grep -o '[0-9]*\.[0-9]*\.[0-9]*')" >> $GITHUB_ENV
+echo "PLUGIN_VERSION<<EOF" >> $GITHUB_ENV
+echo $PLUGIN_VERSION | grep -o '[0-9]*\.[0-9]*\.[0-9]*' >> $GITHUB_ENV
+echo 'EOF' >> $GITHUB_ENV
 
 # Get revision
-export PLUGIN_VERSION_REVISION=$(git rev-list --count HEAD)
-echo "PLUGIN_VERSION_REVISION=(git rev-list --count HEAD)" >> $GITHUB_ENV
+echo "PLUGIN_VERSION_REVISION<<EOF" >> $GITHUB_ENV
+git rev-list --count HEAD >> $GITHUB_ENV
+echo 'EOF' >> $GITHUB_ENV
 
 # Set revision to superzombiefortress.sp
 sed -i -e 's/#define PLUGIN_VERSION_REVISION.*".*"/#define PLUGIN_VERSION_REVISION "'$PLUGIN_VERSION_REVISION'"/g' superzombiefortress.sp
