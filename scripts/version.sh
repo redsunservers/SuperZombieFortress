2a80104bd7a0dba828a48a5d21a3b28537661217
42 <33488710+FortyTwoFortyTwo@users.noreply.github.com>
2020-11-26 00:26:21 +0000

    r

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,10 +7,5 @@ echo "PLUGIN_VERSION<<EOF" >> $GITHUB_ENV
 echo $PLUGIN_VERSION | grep -o '[0-9]*\.[0-9]*\.[0-9]*' >> $GITHUB_ENV
 echo 'EOF' >> $GITHUB_ENV
 
-# Get revision
-echo "PLUGIN_VERSION_REVISION<<EOF"
-git rev-list --count HEAD
-echo 'EOF'
-
 # Set revision to superzombiefortress.sp
 sed -i -e 's/#define PLUGIN_VERSION_REVISION.*".*"/#define PLUGIN_VERSION_REVISION "'$PLUGIN_VERSION_REVISION'"/g' superzombiefortress.sp
