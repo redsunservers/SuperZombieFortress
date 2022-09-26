03f81827683eb34898dc7ad7e6e8a514bf7efb05
FortyTwoFortyTwo <connorrowlanduk@gmail.com>
2022-09-26 21:45:43 +0100

    Fix build from tf2attributes

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,7 @@ cp -r ../addons/sourcemod/scripting addons/sourcemod
 cd addons/sourcemod/scripting
 
 # Install Dependencies
-wget "https://raw.githubusercontent.com/FlaminSarge/tf2attributes/master/tf2attributes.inc" -O include/tf2attributes.inc
+wget "https://raw.githubusercontent.com/FlaminSarge/tf2attributes/master/scripting/include/tf2attributes.inc" -O include/tf2attributes.inc
 wget "https://raw.githubusercontent.com/nosoop/SM-TFEconData/master/scripting/include/tf_econ_data.inc" -O include/tf_econ_data.inc
 wget "https://raw.githubusercontent.com/peace-maker/DHooks2/dynhooks/sourcemod_files/scripting/include/dhooks.inc" -O include/dhooks.inc
 wget "https://www.doctormckay.com/download/scripting/include/morecolors.inc" -O include/morecolors.inc
